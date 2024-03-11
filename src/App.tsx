834d4e223389f7ab9957d8ff37c5702163e5acfa
lordeadroid <rishabhrawat34@gmail.com>
2024-03-11 15:36:53 +0530

    display all files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [books, setBooks] = useState<string[]>([""]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/lib")
+      .then((res) => res.json())
+      .then((data) => {
+        setBooks(data);
+      });
+  }, []);
+
+  const Books: React.JSX.Element[] = books.map(
+    (book: string, index: number) => {
+      return <div key={index}>{book}</div>;
+    },
+  );
+
   return (
     <>
-      <h1>Library</h1>
+      <h1>Rishabh</h1>
+      <>{Books}</>
     </>
   );
 };
