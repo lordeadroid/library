293b373913429602baf6ac21eb28fc4cc2c1a092
lordeadroid <rishabhrawat34@gmail.com>
2024-03-12 17:56:08 +0530

    displaying name of all books

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,28 @@ const App = () => {
   const [books, setBooks] = useState<string[]>([""]);
 
   useEffect(() => {
-    fetch("http://localhost:8000/lib")
+    fetch("https://library-bff.onrender.com/lib")
       .then((res) => res.json())
       .then((data) => {
         setBooks(data);
       });
-  }, []);
+  }, [books]);
 
   const Books: React.JSX.Element[] = books.map(
     (book: string, index: number) => {
-      return <div key={index}>{book}</div>;
+      const bookName: string = book.split(".")[0].replaceAll("-", " ");
+
+      return (
+        <div key={index} className="books">
+          <div>{bookName}</div>
+        </div>
+      );
     },
   );
 
   return (
     <>
-      <h1>Rishabh</h1>
+      <h1>Library</h1>
       <>{Books}</>
     </>
   );
